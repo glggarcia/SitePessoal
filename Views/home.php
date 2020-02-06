2c0f4bf8090d5d6bbc8271ab7ff0a800e3769c22
gabriel <glggarcia2013@gmail.com>
2020-02-06 14:39:14 -0300

    Versão quase final 2

--- Views/home.php
+++ Views/home.php
@@ -74,6 +74,19 @@
             </div>
         </article>
 
+        <article class="section__content2">
+            <h2 class="section__content2--title">Bolsista de Iniciação Científica</h2>
+            <div class="section__div">
+                <div class="section__div--periodo">
+                    <p>Maio de 2016 - Fev-2017 </p>
+                </div>
+                <div class="section__div--descricao">
+                    <p>Pesquisa básica e elaboração de relatórios sobre o tema Tecnologia da Informação Verde. Neste período, foram abordadas as principais técnicas relacionadas ao tema, as organizações que já aplicam em sua estrutura, as principais vantagens e desvantagens e etc...
+                        Ao final do período desta Iniciação, como conclusão, foi elaborado um artigo contendo todo conteúdo abordado e as principais áreas de aplicabilidade da Tecnologia Verde nas organizações e uma apresentação realizada no 11º Seminário de Iniciação Científica e Tecnológica na FAI - Centro de Ensino Superior em Gestão, Tecnologia e Educação.</p>
+                </div>
+            </div>
+        </article>
+
         <h2 class="mb__aux">Profissionais <hr></h2>
         <article class="section__content2">
             <h2 class="section__content2--title">Freelancer - Desenvolvedor PHP</h2>
@@ -96,18 +109,6 @@
                     <p>- Auxílio no desenvolvimento de aplicações internas utilizando as tecnologias  PHP, Java, HTML, CSS e Javascript,
                         manutenção de computadores e servidores e manutenção preventiva. 
                         <br> - Serviço Help Desk.</p>
-                </div>
-            </div>
-        </article>
-        <article class="section__content2">
-            <h2 class="section__content2--title">Bolsista de Iniciação Científica</h2>
-            <div class="section__div">
-                <div class="section__div--periodo">
-                    <p>Maio de 2016 - Fev-2017 </p>
-                </div>
-                <div class="section__div--descricao">
-                    <p>Pesquisa básica e elaboração de relatórios sobre o tema Tecnologia da Informação Verde. Neste período, foram abordadas as principais técnicas relacionadas ao tema, as organizações que já aplicam em sua estrutura, as principais vantagens e desvantagens e etc...
-                        Ao final do período desta Iniciação, como conclusão, foi elaborado um artigo contendo todo conteúdo abordado e as principais áreas de aplicabilidade da Tecnologia Verde nas organizações e uma apresentação realizada no 11º Seminário de Iniciação Científica e Tecnológica na FAI - Centro de Ensino Superior em Gestão, Tecnologia e Educação.</p>
                 </div>
             </div>
         </article>
@@ -289,21 +290,22 @@
 
         <article>
             <h4>
+                <span>Aplicação Web Portal Educacional da Cidadania</span><br>
+                jun. de 2017 – nov. de 2017                </h4>
+                <p>Este protótipo, desenvolvido em 2017, durante o estágio, é uma plataforma online onde professores, educadores e qualquer pessoa ligada a área da educação possam compartilhar conteúdos ligados a Educação Cidadã.
+                Os conteúdos eram compartilhados através de questões para o Ensino Básico sendo, objetivas ou discursivas, e todas elas fundadas na Constituição Federal, levando em consideração linguagem simples e vinculadas as Disciplinas do Ensino Básico.</p>
+                <p>Ele possuía funções como: Login, área administrativa, CRUD de questões com upload e validação e função de avaliação de questões.<br>
+                    Foi desenvolvido utilizando Java (Spring-MVC), Javascript, Jquery, HTML, CSS e Bootstrap.</p>
+                <p>Também possui uma máteria, <a href="http://www.cmpa.mg.gov.br/Noticia/Visualizar/1690" target="_blank"> acessando por este link</a> para mais informações</p>
+        </article>
+
+        <article>
+            <h4>
                 <span>Resenha: Tecnologia da Informação Verde </span><br>
                 Novembro de 2016 - Revista INICIA
             </h4>
             <p>Resenha sobre o artigo Tecnologia da Informação Verde: Um Estudo sobre sua Adoção nas Organizações, dos autores Ana Carolina Salles, Ana Paula Ferreira Alves, Décio Bittencourt Dolci e Guilherme Lerch Lunardi, publicada na Revista INICIA (Revista de Iniciação Científica da FAI 
                 - Centro de Ensino Superior em Gestão, Tecnologia e Educação).</p>
-        </article>
-        <article>
-            <h4>
-                <span>Aplicação Web Portal Educacional da Cidadania</span><br>
-                jun. de 2017 – nov. de 2017                </h4>
-                <p>Este protótipo, desenvolvido em 2017, durante o estágio, é uma plataforma online onde professores, educadores e qualquer pessoa ligada a área da educação possam compartilhar conteúdos ligados a Educação Cidadã.
-                Os conteúdos eram compartilhados através de questões para o Ensino Básico sendo, objetivas ou discursivas, e todas elas fundadas na Constituição Federal, levando em consideração linguagem simples e vinculadas as Disciplinas do Ensino Básico.</p>
-                <p>Este protótipo possuía funções como: Login, área administrativa, CRUD de questões com upload e validação e função de avaliação de questões.<br>
-                    Foi desenvolvido utilizando Java (Spring-MVC), Javascript, Jquery, HTML, CSS e Bootstrap.</p>
-                <p>Também possui uma máteria, <a href="http://www.cmpa.mg.gov.br/Noticia/Visualizar/1690" target="_blank"> acessando por este link</a> para mais informações</p>
         </article>
     </section>
 </main>
